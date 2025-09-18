4aab1caaa12eb586e41ac8299a9f5027ba35701f
Sergiy Fomin <fominssergiy@gmail.com>
2025-09-18 13:26:04 -0400

    feat: add hot reload to be server

--- backend/app/src/routes/ping.routes.ts
+++ backend/app/src/routes/ping.routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify';
 
 export const pingRoutes = async (fastify: FastifyInstance) => {
   fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' });
+    reply.send({ hello: 'world', timestamp: new Date().toISOString() });
   });
 };
