bfb5396469dafa8cde13f803b750c554206750e0
Sergiy Fomin <fominssergiy@gmail.com>
2026-01-15 10:46:59 -0500

    feat: adjust prisma client to default to app schema and add new tables for llm ingridient analysis

--- backend/prisma/client.ts
+++ backend/prisma/client.ts
@@ -1,4 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/prisma/index.js';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({
+  adapter,
+  // log: ['query'], // Logs all SQL queries
+});
 export { prisma };
